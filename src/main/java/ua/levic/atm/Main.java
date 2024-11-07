cfe34cf32e03062df16de62f180fc6c3ede60e18
Viktor Levitskyi <iavenjq21@gmail.com>
2024-11-07 13:45:05 +0200

    created main class

--- src/main/java/ua/levic/atm/Main.java
+++ src/main/java/ua/levic/atm/Main.java
@@ -1,5 +1,20 @@
 package ua.levic.atm;
 
-public class Main {
+import ua.levic.atm.model.Card;
+import ua.levic.atm.model.ExecutiveClass;
 
+import java.util.Scanner;
+
+import static ua.levic.atm.model.ExecutiveClass.getCard;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter your card number : ");
+            Card insertedCard = getCard(scanner.nextLine());
+            ExecutiveClass.getLogicsInfo(insertedCard);
+        }
+    }
 }
